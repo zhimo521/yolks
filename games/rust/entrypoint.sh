9ac782a51b01f83fe7de12d47d678743287674c0
zhimo521 <79039242+zhimo521@users.noreply.github.com>
2024-08-02 08:40:11 +0800

    Update entrypoint.sh

--- games/rust/entrypoint.sh
+++ games/rust/entrypoint.sh
@@ -29,7 +29,7 @@ if [[ "${FRAMEWORK}" == "carbon" ]]; then
 elif [[ "$OXIDE" == "1" ]] || [[ "${FRAMEWORK}" == "oxide" ]]; then
     # Oxide: https://github.com/OxideMod/Oxide.Rust
     echo "Updating uMod..."
-    curl -sSL "https://mirror.ghproxy.com//https://github.com/OxideMod/Oxide.Rust/releases/latest/download/Oxide.Rust-linux.zip" > umod.zip
+    curl -sSL "https://aimei521.oss-cn-beijing.aliyuncs.com/Oxide.Rust-linux.zip" > umod.zip
     unzip -o -q umod.zip
     rm umod.zip
     echo "Done updating uMod!"
